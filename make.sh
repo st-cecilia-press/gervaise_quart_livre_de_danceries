aa50c1fea6bb6d5d918af98f91d7d058e9ca3c4e
Monique Rio <mrio@umich.edu>
2013-04-30 11:06:52 -0400

    renamed things

--- make.sh
+++ make.sh
@@ -5,10 +5,18 @@ LILYPOND="lilypond --include $PWD/include"
 for file in `find . -mindepth 2 -name '*ly' | egrep -v '(include)'`;
 do ly_file=$file;
     folder=`basename "$ly_file" .ly`;
-    pdf_file_orig=${ly_file%.ly}-orig_clef.pdf
-    pdf_file_modern=${ly_file%.ly}-modern_clef.pdf
+
+    pdf_file_orig=${ly_file%.ly}_orig_clef.pdf
+    pdf_file_orig_dash=${ly_file%.ly}-orig_clef.pdf
+
+    pdf_file_modern=${ly_file%.ly}_modern_clef.pdf
+    pdf_file_modern_dash=${ly_file%.ly}-modern_clef.pdf
+
+    midi_file_suffix=${ly_file%.ly}-orig_clef.midi
+    midi_file_no_suffix=${ly_file%.ly}.midi
+
     if [[( ! -e $pdf_file_modern ) || ( ! -e $pdf_file_orig ) || ( $ly_file -nt $pdf_file_modern ) || ($ly_file -nt $pdf_file_orig) ]];
-    then $LILYPOND --output=./$folder $ly_file; echo $LILYPOND;
+    then $LILYPOND --output=./$folder $ly_file; mv $pdf_file_orig_dash $pdf_file_orig; mv $pdf_file_modern_dash $pdf_file_modern; mv $midi_file_suffix $midi_file_no_suffix;
     fi;
 done
 
